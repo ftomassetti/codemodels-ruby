99f46ffc0d485b5b91dbb3eb1dc98710c5e053f3
Federico Tomassetti <f.tomassetti@gmail.com>
2013-08-28 16:05:06 +0200

    using the correct LightModels method for serialization

--- lib/rubymm/model_building.rb
+++ lib/rubymm/model_building.rb
@@ -5,14 +5,14 @@ module RubyMM
 def self.generate_ruby_models_in_dir(src,dest,model_ext='rb.lm',max_nesting=500)
 	LightModels::ModelBuilding.generate_models_in_dir(src,dest,'rb',model_ext,max_nesting) do |src|
 		root = RubyMM.parse_file(src)
-		LightModels::Serialization.eobject_to_model(root)
+		LightModels::Serialization.rgenobject_to_model(root)
 	end
 end
 
 def self.generate_ruby_model_per_file(src,dest,model_ext='rb.lm',max_nesting=500)
 	LightModels::ModelBuilding.generate_model_per_file(src,dest) do |src|
 		root = RubyMM.parse_file(src)
-		LightModels::Serialization.eobject_to_model(root)
+		LightModels::Serialization.rgenobject_to_model(root)
 	end
 end
 
