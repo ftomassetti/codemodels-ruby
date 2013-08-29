509b89e83446c25e7e5658aff6e130cfc0004738
Federico Tomassetti <f.tomassetti@gmail.com>
2013-08-29 14:17:12 +0200

    Error handling for model generation

--- lib/rubymm/model_building.rb
+++ lib/rubymm/model_building.rb
@@ -2,15 +2,15 @@ require 'lightmodels'
 
 module RubyMM
 
-def self.generate_ruby_models_in_dir(src,dest,model_ext='rb.lm',max_nesting=500)
-	LightModels::ModelBuilding.generate_models_in_dir(src,dest,'rb',model_ext,max_nesting) do |src|
+def self.generate_ruby_models_in_dir(src,dest,model_ext='rb.lm',max_nesting=500,error_handler=nil)
+	LightModels::ModelBuilding.generate_models_in_dir(src,dest,'rb',model_ext,max_nesting,error_handler) do |src|
 		root = RubyMM.parse_file(src)
 		LightModels::Serialization.rgenobject_to_model(root)
 	end
 end
 
-def self.generate_ruby_model_per_file(src,dest,model_ext='rb.lm',max_nesting=500)
-	LightModels::ModelBuilding.generate_model_per_file(src,dest) do |src|
+def self.generate_ruby_model_per_file(src,dest,model_ext='rb.lm',max_nesting=500,error_handler=nil)
+	LightModels::ModelBuilding.generate_model_per_file(src,dest,max_nesting,error_handler) do |src|
 		root = RubyMM.parse_file(src)
 		LightModels::Serialization.rgenobject_to_model(root)
 	end
